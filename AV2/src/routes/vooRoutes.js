d044a68280e676cce99716cc121ea6e9f6f6ed30
Natalie Marques <namqsf@gmail.com>
2024-12-04 15:52:39 -0300

    Refact vooRoutes.js

--- AV2/src/routes/vooRoutes.js
+++ AV2/src/routes/vooRoutes.js
@@ -1,47 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Voo = require('../models/Voo');
+const vooController = require('../controllers/vooController');
 
 // Criar um novo voo
-router.post('/', async (req, res) => {
-  try {
-    const voo = await Voo.create(req.body);
-    res.status(201).json(voo);
-  } catch (error) {
-    console.error('Erro ao criar voo:', error); // Adicionando o log detalhado
-    res.status(500).json({ error: 'Erro ao criar voo.', details: error.message });
-  }
-});
-
+router.post('/', vooController.criarVoo);
 
 // Listar todos os voos
-router.get('/', async (req, res) => {
-  try {
-    const voos = await Voo.findAll();
-    res.json(voos);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao listar voos.' });
-  }
-});
+router.get('/', vooController.listarVoos);
 
 // Atualizar um voo
-router.put('/:id', async (req, res) => {
-  try {
-    const voo = await Voo.update(req.body, { where: { id: req.params.id } });
-    res.json(voo);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao atualizar voo.' });
-  }
-});
+router.put('/:id', vooController.atualizarVoo);
 
 // Deletar um voo
-router.delete('/:id', async (req, res) => {
-  try {
-    await Voo.destroy({ where: { id: req.params.id } });
-    res.json({ message: 'Voo deletado com sucesso.' });
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao deletar voo.' });
-  }
-});
+router.delete('/:id', vooController.deletarVoo);
 
 module.exports = router;
